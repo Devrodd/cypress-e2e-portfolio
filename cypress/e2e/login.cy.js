fe89266856d00e9ef0661d4aba66bb9066f99c94
Devrodd <rodrigodantas@gmail.com>
2025-06-11 23:12:00 +0200

    chore: projeto inicial do Cypress E2E Portfolio

---
+++ cypress/e2e/login.cy.js
@@ -0,0 +1,15 @@
+
+describe('Login', () => {
+
+    it('Login com sucesso', () => {
+        
+        cy.visit('/')
+            //get() - selecionar elementos
+            .get('.header-logo')
+
+            // contains() - 
+
+            cy.get('#top_header')
+
+    })
+})
