1fd1d0f68dc1b3236b45413cce5ff86fbeb33f11
Devrodd <rodrigodantas@gmail.com>
2025-06-12 00:05:24 +0200

    feat(e2e): adicionar exemplos de get/contains/find em login.cy.js

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -7,9 +7,19 @@ describe('Login', () => {
             //get() - selecionar elementos
             .get('.header-logo')
 
-            // contains() - 
+            // contains() - encontrar elementos por textos
 
-            cy.get('#top_header')
+        cy.get('#top_header').as('cabecalho')
+            .contains('Login')
 
+            // find() - encontra elementos
+
+        cy.get('cabecalho')
+            .find('.fa-user')
+
+           //as() - alias 
+
+           cy.get('cabecalho')
+           .find('.fa-user')
     })
 })
